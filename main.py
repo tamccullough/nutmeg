4dbc2505510de38cec6ec333801e8f4e89809590
tamccullough <howdytodd@gmail.com>
2020-07-11 17:05:32 -0400

    added new player page

--- main.py
+++ main.py
@@ -380,6 +380,23 @@ def player():
     crest = roster_team_info.iloc[0][5]
     return render_template('cpl-es-player.html', name = name,team_name = team, html_table = player, team_colour = roster_colour, year = year, crest = crest, other_year = other_year)
 
+@canples.route('/player-2019', methods=['POST'])
+def player_19():
+    year = '2019'
+    other_year = '2020'
+    stats = pd.read_csv(f'datasets/{year}/cpl-{year}-stats.csv')
+    team_ref = pd.read_csv('datasets/teams.csv')
+    player_info = pd.read_csv(f'datasets/{year}/player-{year}-info.csv')
+
+    name = request.form['name']
+
+    player = cpl_main.get_player_card(name,stats,player_info)
+    team = player['team'].values[0]
+    roster_team_info = team_ref[team_ref['team'] == team]
+    roster_colour = roster_team_info.iloc[0][4]
+    crest = roster_team_info.iloc[0][5]
+    return render_template('2019/cpl-es-player.html', name = name,team_name = team, html_table = player, team_colour = roster_colour, year = year, crest = crest, other_year = other_year)
+
 @canples.route('/goals')
 def goals():
     year = '2020'
